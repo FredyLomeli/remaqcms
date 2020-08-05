7c4bb6aabfba85433081cce09d1a62864f65b6c2
Fredy Lomelí <ing.lomeli@gmail.com>
2020-08-04 22:58:15 -0500

    basic desing

---
+++ database/factories/InformationFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\information;
+use Faker\Generator as Faker;
+
+$factory->define(information::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
